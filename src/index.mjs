4ce686adf8b6d37b5e386ead17250b733d221c37
Naoto Kaimoto <73768470+naoto911@users.noreply.github.com>
2023-12-29 12:22:16 +0000

    map,filterについて

--- src/index.mjs
+++ src/index.mjs
@@ -192,3 +192,38 @@
 // console.log(arr8);
 
 // console.log(arr4);
+
+// mapやfilterを使った配列の処理
+// const nameArr = ["田中", "山田", "佐藤"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(nameArr[index]);
+// }
+
+// nameArr.map((name) => {
+//   console.log(name);
+// });
+// nameArr.map((name) => console.log(name));
+
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// });
+// console.log(nameArr2);
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+// const nameArr = ["田中", "山田", "佐藤"];
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です。`));
+
+// const nameArr = ["田中", "山田", "佐藤"];
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "佐藤") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
