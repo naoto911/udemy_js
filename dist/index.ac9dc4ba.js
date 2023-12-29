1850c9145592d345fe0db70094e8c54014a14cd4
Naoto Kaimoto <73768470+naoto911@users.noreply.github.com>
2023-12-29 11:11:55 +0000

    分割代入について

--- dist/index.ac9dc4ba.js
+++ dist/index.ac9dc4ba.js
@@ -659,6 +659,22 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 //   fuge: num2,
 // });
 // console.log(func3(1, 2));
+// 分割代入
+// const myProfile = {
+//   name: "naoto",
+//   age: 26,
+// };
+// // const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// // console.log(message1);
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+// const myProfile = ["naoto", 26];
+// const message1 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// console.log(message1);
+// const [name, age] = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
 
 },{}]},["iZQ9B","aR1JP"], "aR1JP", "parcelRequirebaba")
 
