c8f1b2ae279402983575b98a354f4c04f3a52c93
Naoto Kaimoto <73768470+naoto911@users.noreply.github.com>
2023-12-29 11:54:28 +0000

    スプレット構文

--- dist/index.ac9dc4ba.js
+++ dist/index.ac9dc4ba.js
@@ -692,6 +692,37 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 // const age = 26;
 // const myProfile = { name, age };
 // console.log(myProfile);
+// スプレット構文
+// 配列の展開
+// const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
+// const arr1 = [1, 2];
+// const sumFunction = (num1, num2) => console.log(num1 + num2);
+// // sumFunction(arr1[0], arr1[1]);
+// sumFunction(...arr1);
+// まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+// 配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+// 配列のコピー
+// const arr6 = [...arr4];
+// console.log(arr4);
+// arr6[0] = 100;
+// console.log(arr6);
+// console.log(arr4);
+// 配列の結合
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+// const arr8 = arr4;
+// arr8[0] = 100;
+// console.log(arr8);
+// console.log(arr4);
 
 },{}]},["iZQ9B","aR1JP"], "aR1JP", "parcelRequirebaba")
 
