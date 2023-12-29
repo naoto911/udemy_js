4f30ba4f01092ca9dd6b56a1c883ce1bf1ba2986
Naoto Kaimoto <73768470+naoto911@users.noreply.github.com>
2023-12-29 09:20:13 +0000

    var,const,letの挙動の確認

--- dist/index.ac9dc4ba.js
+++ dist/index.ac9dc4ba.js
@@ -575,11 +575,46 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"aR1JP":[function(require,module,exports) {
-var _stylesCss = require("./styles.css");
-document.getElementById("app").innerHTML = `
-<h1>Hello JavaScript!</h1>
-`;
+// var val1 = "var変数";
+// console.log(val1);
+// // var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+// // var変数は際宣言可能
+// var val1 = "var変数を際宣言";
+// console.log(val1);
+// let val2 = "let変数";
+// console.log(val2);
+// // letは上書きは可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+// letは際宣言不可能
+// let val2 = "let変数を際宣言";
+// console.log(val2);
+// const val3 = "const変数";
+// console.log(val3);
+// const変数は上書き不可
+// val3 = "const変数を上書き";
+// const変数は上書き不可
+// sonst val3 = "const変数を際宣言";
+// constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "naoto",
+//   age: 26,
+// };
+// console.log(val4);
+// val4.name = "NAOTO";
+// val4.age = 27;
+// val4.address = "Tokyo";
+// console.log(val4);
+// constで定義した配列はプロパティの変更が可能
+// const val5 = ["dog", "cat"];
+// console.log(val5);
+// val5[0] = "bird";
+// console.log(val5);
+// val5.push("monkey");
+// console.log(val5);
 
-},{"./styles.css":"lW6qc"}],"lW6qc":[function() {},{}]},["iZQ9B","aR1JP"], "aR1JP", "parcelRequirebaba")
+},{}]},["iZQ9B","aR1JP"], "aR1JP", "parcelRequirebaba")
 
 //# sourceMappingURL=index.ac9dc4ba.js.map
